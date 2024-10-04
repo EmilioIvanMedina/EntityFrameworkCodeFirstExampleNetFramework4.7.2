0ca7c711cded74d4c03eb905aaf95a92aaff6f5a
Emilio Medina Martinez <emilio.medinamartinez@gmail.com>
2024-10-04 20:10:02 -0300

    implement repository interfaces

---
+++ EntityFrameworkCodeFirstExample/CustomerMessageRepository/Entities/CustomerMessage.cs
@@ -0,0 +1,13 @@
+﻿namespace CustomerMessageRepository.Entities
+{
+    public class CustomerMessage
+    {
+        public int IdCustomerMessage { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Message { get; set; }
+    }
+}
