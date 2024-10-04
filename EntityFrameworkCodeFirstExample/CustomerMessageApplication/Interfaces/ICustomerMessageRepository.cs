cf9e5316badbda1b3c90f5bd267c1d043786e299
Emilio Medina Martinez <emilio.medinamartinez@gmail.com>
2024-10-04 18:10:09 -0300

    Add application project + interfaces + DTOs

---
+++ EntityFrameworkCodeFirstExample/CustomerMessageApplication/Interfaces/ICustomerMessageRepository.cs
@@ -0,0 +1,10 @@
+﻿using CustomerMessageApplication.DTOs;
+using System.Threading.Tasks;
+
+namespace CustomerMessageApplication.Interfaces
+{
+    public interface ICustomerMessageRepository
+    {
+        Task SaveMesssage(CustomerMessageDTO message);
+    }
+}
