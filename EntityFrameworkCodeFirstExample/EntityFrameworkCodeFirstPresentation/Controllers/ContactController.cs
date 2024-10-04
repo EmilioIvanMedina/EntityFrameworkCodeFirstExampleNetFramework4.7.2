be8f1714bbd13c9df22d699b37b9581805500f5d
Emilio Medina Martinez <emilio.medinamartinez@gmail.com>
2024-10-04 16:45:01 -0300

    Contact page has a message box section

---
+++ EntityFrameworkCodeFirstExample/EntityFrameworkCodeFirstPresentation/Controllers/ContactController.cs
@@ -0,0 +1,30 @@
+﻿using EntityFrameworkCodeFirstPresentation.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EntityFrameworkCodeFirstPresentation.Controllers
+{
+    public class ContactController : Controller
+    {
+        public ActionResult Index()
+        {
+            var model = new ContactMessageViewModel();
+            ViewBag.Message = "Your contact page.";
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Index(ContactMessageViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                ViewBag.Message = "Your message has been sent!";
+            }
+
+            return View(model);
+        }
+    }
+}
