37a1cffade1ce910909a8d0257acca0ae340099a
Emilio Medina Martinez <emilio.medinamartinez@gmail.com>
2024-10-05 00:47:20 -0300

    creating database model, dbcontext, applying migrations to create db

---
+++ EntityFrameworkCodeFirstExample/CustomerMessageRepository/DatabaseModel/CustomerMessageDbContext.cs
@@ -0,0 +1,25 @@
+﻿using CustomerMessageRepository.Entities;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+
+namespace CustomerMessageRepository.DatabaseModel
+{
+    public class CustomerMessageDbContext : DbContext
+    {
+        public DbSet<CustomerMessage> CustomerMessages { get; set; }
+
+        public CustomerMessageDbContext() : base("name=CustomerMessageDbConnection")
+        {
+            
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CustomerMessage>()
+                .HasKey(cm => cm.IdCustomerMessage)
+                .Property(cm => cm.IdCustomerMessage)
+                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
